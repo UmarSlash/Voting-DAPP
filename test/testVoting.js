88953d16a49c7840964589f34835a07f3038f0bc
UmarSlash <97157561+UmarSlash@users.noreply.github.com>
2024-01-20 07:22:13 +0800

    Add files via upload

--- test/testVoting.js
+++ test/testVoting.js
@@ -1,10 +1,39 @@
-var artifacts = require('truffle-artifactor');
 const Voting = artifacts.require("Voting");
 
-contract('Voting', () => {
-  it("Should deploy smart contract properly", async() => {
-    const testVoting = await Voting.deployed();
-    console.log(testVoting.address);
-    assert(testVoting.address !== '');
-    });
+contract('Voting', (accounts) => {
+  let votingInstance;
+
+  beforeEach(async () => {
+    votingInstance = await Voting.deployed();
   });
+
+  it("Should deploy smart contract properly", async () => {
+    assert(votingInstance.address !== '');
+  });
+
+  it("Should allow only admin to start voting", async () => {
+    try {
+      await votingInstance.startVoting({ from: accounts[1] });
+      assert.fail("Non-admin should not be able to start voting");
+    } catch (error) {
+      assert(error.reason.includes("Only the admin can execute this"));
+    }
+
+    await votingInstance.startVoting({ from: accounts[0] });
+    const isOpen = await votingInstance.votingOpen();
+    assert.isTrue(isOpen, "Voting should be open");
+  });
+
+  it("Should allow only admin to stop voting", async () => {
+    try {
+      await votingInstance.stopVoting({ from: accounts[1] });
+      assert.fail("Non-admin should not be able to stop voting");
+    } catch (error) {
+      assert(error.reason.includes("Only the admin can execute this"));
+    }
+
+    await votingInstance.stopVoting({ from: accounts[0] });
+    const isOpen = await votingInstance.votingOpen();
+    assert.isFalse(isOpen, "Voting should be closed");
+  });
+});
